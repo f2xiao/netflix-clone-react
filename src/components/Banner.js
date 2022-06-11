649c167620a0c24dbcd58bee8f94bba6bdf3d1e9
f2xiao <fxiao.mecheng@gmail.com>
2022-06-11 18:02:57 -0400

    Add Banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Banner() {
+  return (
+    <BannerContainer>
+      <BannerContents>
+      <h1>Movie Name</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum qui eum nemo, voluptatum ad, atque, explicabo reprehenderit magnam odit inventore iure veritatis doloremque quis consequatur. Ad molestias repudiandae animi iste.</p>
+      <div>
+        <button>Play</button>
+        <button>More info</button>
+      </div>
+      </BannerContents>
+    </BannerContainer>
+  )
+}
+
+export default Banner
+
+const url = 'https://thelesfilms.files.wordpress.com/2016/06/netflix-banner.jpg';
+
+const BannerContainer = styled.div`
+  background: url(${url});
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  padding-bottom: 2.5em;
+`
+
+const BannerContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  width:40%;
+  margin-left: 2.5em;
+  >h1{
+    padding-top: 2em;
+  }
+  >p{
+    padding: 1em 0;
+  }
+`
