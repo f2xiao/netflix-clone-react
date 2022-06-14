8d275d19f3a09b8ee7f198496988d9968fb75772
f2xiao <fxiao.mecheng@gmail.com>
2022-06-14 16:10:10 -0400

    Improve button iteration animations

--- src/components/Row.js
+++ src/components/Row.js
@@ -5,27 +5,32 @@ import axios from '.././axios'
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
-  const [show, setShow] = useState(false);
+  const [showNext, setShowNext] = useState(false);
+  const [showPrev, setShowPrev] = useState(false);
   const postersContainer = useRef();
+  const postersElement = postersContainer.current;
   const prevButton = useRef();
+  const imgElement = useRef();
   const imgbase_url = `https://image.tmdb.org/t/p/original/`;
+
+  
   useEffect(() => {
     const fetchMovies = async () => {
       const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
+      console.log(request.data.results.length)
     }
     fetchMovies();
   }, [fetchUrl]);
 
   const slide = (button) => {
-    // console.log(e.target.parentNode.parentNode.children[1].scrollLeft)
     
     let distanceX;
     if (button === 'next') {
-      distanceX =postersContainer.current.scrollLeft + 400
+      distanceX =postersElement.scrollLeft +  postersElement.offsetWidth
     }
     if (button === 'prev') {
-      distanceX =postersContainer.current.scrollLeft - 400
+      distanceX =postersElement.scrollLeft -  postersElement.offsetWidth
     }
 
     postersContainer.current.scroll({
@@ -33,24 +38,49 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
       behavior:'smooth'
     })
   }
+  const handleShowButton = () => {
+    const start = window.innerWidth;
+    const end = postersElement.scrollWidth;
+    const range = end - start;
+    const distance = postersElement.scrollLeft;
+    // console.log(`start is ${start}, range is ${range}, end is ${end}`);
+    // console.log(`scrollLeft is ${distance}`);
+    if ( distance < range) { 
+      setShowNext(true);
+    }
+    if (distance !== 0) {
+      setShowPrev(true);
+    }
+  
+  }
   const handleClickPrev = () => {
+    if (postersElement.scrollLeft < postersElement.offsetWidth) {
+      setShowPrev(false);
+    }
+    setShowNext(true);
     slide('prev');
   }
   const handleClickNext = () => {
-    prevButton.current.style.opacity = 0.8;
+    if (postersElement.scrollLeft > postersElement.scrollWidth - window.innerWidth - postersElement.offsetWidth) {
+      setShowNext(false);
+    }
+    setShowPrev(true);
     slide('next')
   }
   return (
     <RowContainer>
       <h2>{title}</h2>
-      <Posters ref={postersContainer} onMouseEnter={()=>{setShow(true)}} onMouseLeave={()=>{setShow(false)}}>
+      <Posters
+        ref={postersContainer}
+        onMouseEnter={() => { handleShowButton() }}
+        onMouseLeave={() => { setShowNext(false); setShowPrev(false) }}>
         {movies.map(movie =>
-          <img className={isLargeRow? 'large' : undefined}
+          <img ref={imgElement} className={isLargeRow? 'large' : undefined}
             key={movie.id}
             alt={movie.title}
             src={`${imgbase_url}${isLargeRow ? (movie?.poster_path) : (movie?.backdrop_path)}`} />)}
-        <button ref={prevButton} className={isLargeRow? 'large prev' : 'prev'} onClick={handleClickPrev}>&#10094;</button>
-         <button className={ show && (isLargeRow? 'large next black' : 'next black')} onClick={handleClickNext}>&#10095;</button>
+        <button ref={prevButton} className={showPrev && (isLargeRow? 'large prev black' : 'prev black')} onClick={handleClickPrev}>&#10094;</button>
+         <button className={ showNext && (isLargeRow? 'large next black' : 'next black')} onClick={handleClickNext}>&#10095;</button>
       </Posters>
     </RowContainer>
    
@@ -61,18 +91,19 @@ export default Row
 
 const RowContainer = styled.div`
 position: relative;
-
 padding-left: var(--padding-left);
 >h2{
   padding:0.5em 0;
 }
 
+
+
 `
 const Posters = styled.div`
-
   display:flex;
   flex-direction: row;
-  overflow: auto;
+  overflow-y: hidden;
+  overflow-x: scroll;
   /* padding: 0 1.5em; */
   &::-webkit-scrollbar {
   display:none
@@ -100,16 +131,17 @@ const Posters = styled.div`
   position: absolute;
   top: 56px;
   opacity: 0;
+  font-size:2em;
   &.black{
-    opacity: 0.8;
+    opacity: 0.7;
   }
 
   text-align: center;
 
   height: 100px;
-  width: 50px;
+  width: 60px;
   border:none;
-  /* transition: all 0.5s ease-in; */
+  border-radius:4px;
   &.large{
     height: 250px;
   }
@@ -117,7 +149,7 @@ const Posters = styled.div`
   cursor: pointer;
 
   &.prev{
-    left:2em;
+    left:0.5em;
   }
 
   &.next{
