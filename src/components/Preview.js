b077763036dacebfcd3f09d47c67f5ddc14a063a
f2xiao <fxiao.mecheng@gmail.com>
2022-06-17 17:17:58 -0400

    Embeded video for Preview comp

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styled from 'styled-components';
 
-function Preview({ preview, handleMouseLeave }) {
 
+function Preview({ preview, handleMouseLeave }) {
+  const ytBaseUrl = "https://www.youtube.com/embed/";
+  const previewElement = useRef(null);
+  const videoElement = useRef(null);
+
+  const expandPreview = (e) => {
+    previewElement.current.style.position = "fixed";
+    previewElement.current.style.width = "90vw";
+    previewElement.current.style.top = `3em`;
+    previewElement.current.style.left = `5vw`;
+    previewElement.current.style.height = "90vh";
+    previewElement.current.style.fontSize = "1.5em";
+    videoElement.current.style.width = "90vw";
+    videoElement.current.style.height = "80vh";
+  }
   return (
     <PreviewContainer
+      ref={previewElement}
       onMouseLeave={(e)=>{handleMouseLeave(e)}}
       style={{
         top: preview?.position?.top,
@@ -12,14 +27,32 @@ function Preview({ preview, handleMouseLeave }) {
         opacity: preview?.opacity,
         width: preview?.size?.width,
         height: preview?.size?.height,
+        position: preview?.position,
+        fontSize: preview?.fontSize
       }}
-      >
-      
-        <img alt={preview?.movie?.name || preview?.movie?.original_title} src={preview?.imgSrc} />
+    >
+      {preview.video ? (
+        <div>
+          <iframe
+          ref={videoElement}
+          src={`${ytBaseUrl}${preview.video.key}?mute=1&autoplay=1 `}
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+          />
+        </div>
+      ) : (<img alt={preview?.movie?.name || preview?.movie?.original_title} src={preview?.imgSrc} />)}
         <PreviewInfo>
           <h5>
-            {preview?.movie?.name || preview?.movie?.original_title }
-          </h5>
+          {preview?.movie?.name || preview?.movie?.original_title}
+          { preview.video &&
+            <button onClick={(e) => {expandPreview(e)}}>
+            <i className="arrow down" />
+          </button>
+          }
+        </h5>
+        
           <p>
           {preview?.genres}
           </p>
@@ -29,31 +62,68 @@ function Preview({ preview, handleMouseLeave }) {
   )
 }
 
-export default Preview
+export default Preview;
 
 const PreviewContainer = styled.div`
   opacity: 0;
+  background: rgba(255,255,255,1);
   position: absolute;
   font-size:0.8em;
   /* z-index:3; */
   /* transition: all 0.5s; */
-  border-radius:5px;
+  /* border-radius:5px; */
+  border-top-left-radius:5px;
+  border-top-right-radius:5px;
   box-shadow: 5px 5px rgba(255,255,255,0.3);
   will-change: opacity, top, left;
- >img{
+ >img, >div{
   display:block;
   width:100%;
+ }
+ >div{
+  background-color: rgba(0,0,0,0.9);
+  /* position: relative; */
+  z-index:2;
+   >iframe{
+    display:block;
+    width:100%;
+   }
  }
  
  
 `
 const PreviewInfo = styled.div`
   background: rgba(255,255,255,1);
-  padding:0.5em 0.5em;
-  height: 50%;
-  width:100%;
+  padding: 0.5em 0.5em;
  >h5{
-  padding:0.5em 0.5em;
+  position: relative;
+  padding: 0 0.5em;
+  font-size: 1em;
+  height: 2em;
+  line-height: 2em;
+  >button{
+      position: absolute;
+      right: 0.5em;
+      top: 0.5em;
+      padding-top:2px;
+      padding-bottom:5px;
+      height:32px;
+      width:32px;
+      border:2px solid rgba(0,0,0,0.9);
+      border-radius: 50%;
+      >i {
+          &.arrow {
+        border: solid black;
+        border-width: 0 3px 3px 0;
+        display: inline-block;
+        padding: 3px;
+        }
+        &.down {
+        transform: rotate(45deg);
+        -webkit-transform: rotate(45deg);
+        }
+      }
+      }
  }
  >p{
   padding:0.5em 0.5em;
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -47,7 +47,19 @@ function HomeView() {
     },
   ];
 
-
+  const fetchVideoInfo = async (id, type) => {
+    const url = `${type}/${id}/videos?api_key=${api_key}&language=en-US`;
+    let response;
+    try {
+      response = await axios.get(url, {validateStatus: (status) => status === 200});
+      if (response.data.results.length === 0) {
+        return null;
+      }
+    } catch (error) {
+      return null;
+    } 
+    return response;
+  }
   
   const showPreview = async (e, movie) => {
     // preview info: movie genres
@@ -56,19 +68,28 @@ function HomeView() {
       const found = genresList.find((genre) => genre.id === id);
       genres += `${found.name} `;
     });
-    // prview header img url
-    let video = [];
-    Promise.all([
-      axios.get(`movie/${movie.id}/videos?api_key=${api_key}&language=en-US`),
-      axios.get(`tv/${movie.id}/videos?api_key=${api_key}&language=en-US`)
-    ]).then((response) => {
-      video = response[0].data.results.length !== 0 ? response[0].data.results : response[1].data.results
-    }).catch(error => console.log(error));
     
+    // fetch video info
+    let videoInfo1 = await fetchVideoInfo(movie.id, "movie");
+    let videoInfo2 = await fetchVideoInfo(movie.id, "tv");
+    let videoInfo = videoInfo1 ? videoInfo1 : videoInfo2;
+    let video;
+    if (videoInfo?.data) {
+      video = videoInfo?.data.results.find(ele => ele.type === "Trailer" || ele.type === "Teaser");
+      if (!video) {
+        video = videoInfo?.data.results[0]
+      }
+    }
+    // console.log(video)
+    
+    // console.log(videoInfo?.data?.results, movie.id);
+    
+    // console.log(foundVideo);
+    // prview header img url
     let imgSrc = e.target.src;
     
     // preview locations
-    let { top, left, width, height, right } = e.target.getBoundingClientRect();
+    let { top, left, width, height, right,bottom } = e.target.getBoundingClientRect();
     // console.log(e.target.getBoundingClientRect());
     
     // console.log(right, window.innerWidth, width, height);
@@ -76,16 +97,20 @@ function HomeView() {
     if (right > window.innerWidth) {
       left -= width;
     }
+    if (bottom > window.innerHeight) {
+      top -= height + 56;
+    }
+   
     left -= 1 / 4 * width;
-    top = top - 56 +  window.scrollY;
+    top = top - 56 + window.scrollY;
+    
     let position = { top, left };
     let opacity = 1;
-    setPreview({ movie, genres, imgSrc, video, size, position, opacity })
+    setPreview({ movie, genres, imgSrc,video, size, position, opacity })
     // console.log(preview)
   }
-
   const hidePreview = (e) => {
-    setPreview({opacity:0})
+    setPreview({opacity:0, position:"absolute", fontSize:'0.8em'})
   }
 
   useEffect(() => { 
@@ -99,7 +124,7 @@ function HomeView() {
    }, [])
 
     return (
-    <div>
+    <div style={{paddingBottom:'4em'}}>
       <Nav />
         <Banner />
         {fetchRows.map(row => {
